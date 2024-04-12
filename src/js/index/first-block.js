300b09643f99bdf471c0a6c78b3a6521577ef698
Emilahm999 <emilhika777@gmail.com>
2024-04-12 16:35:55 +0500

    Cards items

--- src/js/index/first-block.js
+++ src/js/index/first-block.js
@@ -1,17 +1,8 @@
 import ObserverClass from "../_classes/ObserverClass.js";
 import { PopUp } from "../_classes/Pop-up.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector(".first-block__button");
-
-  new PopUp({
-    title: " Вызвать специалиста на бесплатный замер ",
-    buttonsActivate: button,
-    // targetValue: "3",
-  })
-
-  new ObserverClass(".first-block", ".title");
-  new ObserverClass(".first-block", ".sub-title");
-  new ObserverClass(".first-block", ".first-block__callback-block");
-  new ObserverClass(".first-block", ".first-block__arrow-down");
-})
+// var swiper = new Swiper(".akcii-swiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
