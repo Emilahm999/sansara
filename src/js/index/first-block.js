ed7b7b97c71730549859b1e64e0e5fc0e59643ab
Emilahm999 <emilhika777@gmail.com>
2024-04-16 17:10:33 +0500

    description-block

--- src/js/index/first-block.js
+++ src/js/index/first-block.js
@@ -1,8 +1,11 @@
-import ObserverClass from "../_classes/ObserverClass.js";
-import { PopUp } from "../_classes/Pop-up.js";
-
-// var swiper = new Swiper(".akcii-swiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-// });
+// import ObserverClass from "../_classes/ObserverClass.js";
+// import { PopUp } from "../_classes/Pop-up.js";
+var swiper = new Swiper(".cta__swiper", {
+    pagination: {
+      el: ".cta__pagination",
+    },
+    navigation: {
+      nextEl: ".cta__arrow.next",
+      prevEl: ".cta__arrow.prev",
+    },
+  });
